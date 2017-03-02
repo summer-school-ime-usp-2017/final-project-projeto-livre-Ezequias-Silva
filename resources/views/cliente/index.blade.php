0510407349ec3bf91ba5e79382286febc2eab602
Ezequias_Silva <ezequias91@gmail.com>
2017-03-02 06:51:07 -0800

    Atualizando o index do 'cliente'

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -3,3 +3,55 @@
 @section('title','Clientes')
 
 @section('pager-header-content','Clientes')
+
+@section('content')
+
+  <div class="row">
+    <div class="col-md-3">
+      <div class="panel panel-primary">
+        <div class="panel-heading"><h2 class="panel-title">Clientes cadastrados</h2>
+        </div>
+        <div class="panel-body">
+          <a href="/clientes/cria">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Cadastrar cliente
+          </a>
+        </div>
+
+        <div class="col-md-6">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>CPF</th>
+                <th>Data Nasc.</th>
+                <th>Rua</th>
+                <th>Numero</th>
+                <th>Bairro</th>
+                <th>Cidade</th>
+              </tr>
+            </thead>
+            @foreach ($clientes as $cliente)
+              <tr>
+                <td>{{ $cliente->id }}</td>
+                <td>{{ $cliente->cli_nome}}</td>
+                <td>{{ $cliente->cli_cpf }}</td>
+                <td>{{ $cliente->cli_dataNasc }}</td>
+                <td>{{ $cliente->cli_rua }}</td>
+                <td>{{ $cliente->cli_numero }}</td>
+                <td>{{ $cliente->cli_bairro }}</td>
+                <td>{{ $cliente->cidade }} </td>
+              </tr>
+            @endforeach
+
+          </table>
+
+        </div>
+
+
+      </div>
+
+    </div>
+
+  </div>
+@endsection
