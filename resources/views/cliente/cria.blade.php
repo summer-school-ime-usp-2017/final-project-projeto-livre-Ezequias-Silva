742551dab0954039e75da46a88c9a00b99b7e31c
Ezequias_Silva <ezequias91@gmail.com>
2017-03-02 10:49:30 -0800

    Atualizando o master.blade.php

--- resources/views/cliente/cria.blade.php
+++ resources/views/cliente/cria.blade.php
@@ -29,37 +29,37 @@
 
         <div class="form-group">
           <label for="cli_nome">Nome</label>
-          <input type="text" name="cli_nome" id="cli_nome" class="form-control">
+          <input type="text" name="cli_nome" id="cli_nome" placeholder="Digite seu nome" class="form-control">
         </div>
 
         <div class="form-group">
           <label for="cli_cpf">CPF</label>
-          <input type="text" name="cli_cpf" id="cli_cpf" class="form-control">
+          <input type="text" name="cli_cpf" id="cli_cpf" placeholder="Digite seu CPF" class="form-control">
         </div>
 
         <div class="form-group">
           <label for="cli_dataNasc">Data Nasc.</label>
-          <input type="text" name="cli_dataNasc" id="cli_dataNasc" class="form-control">
+          <input type="text" name="cli_dataNasc" id="cli_dataNasc" placeholder="Digite a data de nascimento" class="form-control">
         </div>
 
         <div class="form-group">
           <label for="cli_rua">Rua</label>
-          <input type="text" name="cli_rua" id="cli_rua" class="form-control">
+          <input type="text" name="cli_rua" id="cli_rua" placeholder="Digite a rua" class="form-control">
         </div>
 
         <div class="form-group">
           <label for="cli_numero">Número</label>
-          <input type="text" name="cli_numero" id="cli_numero" class="form-control">
+          <input type="text" name="cli_numero" id="cli_numero" placeholder="Digite o número" class="form-control">
         </div>
 
         <div class="form-group">
           <label for="cli_bairro">Bairro</label>
-          <input type="text" name="cli_bairro" id="cli_bairro" class="form-control">
+          <input type="text" name="cli_bairro" id="cli_bairro" placeholder="Digite o bairro" class="form-control">
         </div>
 
         <div class="form-group">
           <label for="cidade">Cidade</label>
-          <input type="text" name="cidade" ="cidade" class="form-control">
+          <input type="text" name="cidade" ="cidade" placeholder="Digite a cidade" class="form-control">
         </div>
       <button class="btn btn-primary">Salvar</button>
       </form>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,38 @@
   </head>
   <body>
 
+    <!-- Cabeçalho -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Reservas Instantâneas</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Senha" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Entrar</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1 align="center">Reserve aqui!</h1>        
+      </div>
+    </div>
+
     <div class="container">
 
       <div class="pager-header">
@@ -14,6 +46,10 @@
       </div>
 
       @yield('content')
+
+      <footer>
+        <p>&copy; 2018 Software&Company, Inc.</p>
+      </footer>
 
     </div>
 
